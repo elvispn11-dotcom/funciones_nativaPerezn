3aeb70125824be2784396ac96a24e6516e28c6e1
Elvis javier Perez Nuñez <elvispn11@gmail.com>
2026-03-23 14:02:01 -0600

    funcioncocount

---
+++ funciones_nativasphp/Funciones_nativas.php
@@ -0,0 +1,9 @@
+<?php
+// Buscar funciones nativas de php
+// Meter todas las funciones de php
+// Buscar funciones
+// LAS mas importantes
+// 50
+
+// manipulacion de arreglos son el corazon de php
+count(); // contar elementos de un arreglo
