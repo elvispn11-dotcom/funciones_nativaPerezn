04a551741b38e4eaeb9c35581a62c1bf298089e2
Elvis javier Perez Nuñez <elvispn11@gmail.com>
2026-03-23 14:07:40 -0600

    in_array

--- funciones_nativasphp/Funciones_nativas.php
+++ funciones_nativasphp/Funciones_nativas.php
@@ -14,3 +14,4 @@ array_values(); // obtener los valores de un arreglo
 array_push(); // agregar elementos al final de un arreglo
 array_pop(); // eliminar el último elemento de un arreglo\
 array_shift(); // eliminar el primer elemento de un arreglo
+in_array(); // verificar si un valor existe en un arreglo
