54a4dbb5a1f8f7c89a68426083a1ac0ee9855176
Elvis javier Perez Nuñez <elvispn11@gmail.com>
2026-03-23 14:08:28 -0600

    array_key_exists

--- funciones_nativasphp/Funciones_nativas.php
+++ funciones_nativasphp/Funciones_nativas.php
@@ -15,3 +15,4 @@ array_push(); // agregar elementos al final de un arreglo
 array_pop(); // eliminar el último elemento de un arreglo\
 array_shift(); // eliminar el primer elemento de un arreglo
 in_array(); // verificar si un valor existe en un arreglo
+array_key_exists(); // verificar si una clave existe en un arreglo
